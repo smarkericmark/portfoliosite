c4574d371634a75ff86abd138cfad41458075f4c
smarkericmark <markericberg@gmail.com>
2022-03-04 17:28:46 -0500

    Second commit

--- TestSite.py
+++ TestSite.py
@@ -4,26 +4,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template("home.html")
+	return render_template("layout.html")
 
-@app.route('/about/')
-def about():
-	return render_template("about.html")
-
-@app.route('/eduCerts/')
-def eduCerts():
-	return render_template("eduCerts.html")
-
-@app.route('/skills/')
-def skills():
-	return render_template("skills.html")
-
-@app.route('/projects/')
-def projects():
-	return render_template("projects.html")
-
-
-
-	
 if __name__=="__main__":
 	app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -50,3 +50,115 @@ function aboutClickThree() {
 
 }
 
+
+//////////////
+/ Education /
+/////////////
+
+function eduClickOne() {
+	//document.getElementById("aboutBox").innerHTML = "While working as a Client Service Advisor, I directly aided a corporate client in recovering over $100,000 in stolen funds after the CEO's email was compromised. That incident drew my interest towards Information Security. That interest became a hobby and then a passion. In 2020 I decided to realign my career towards tech while leveraging self-learning, traditional education, and 5+ years of corporate finance experience.";
+	//document.getElementById("aboutIntroBlurb").element.className = "hidden";
+	document.getElementById("eduBlurbOne").classList.add('nothidden');
+	document.getElementById("eduBlurbOne").classList.remove('hidden');
+
+
+	document.getElementById("eduIntroBlurb").classList.add('hidden');
+	document.getElementById("eduIntroBlurb").classList.remove('nothidden');
+	document.getElementById("eduBlurbTwo").classList.add('hidden');
+	document.getElementById("eduBlurbTwo").classList.remove('nothidden');
+	document.getElementById("eduBlurbThree").classList.add('hidden');
+	document.getElementById("eduBlurbThree").classList.remove('nothidden');
+
+	
+}
+
+function eduClickTwo() {
+	//document.getElementById("aboutBox").innerHTML = "Before learning to pursue a tech-focused career, I was a finance professional. I started as a part-time teller. Within five years, I was traveling across the states to meet with high-profile commercial clients and present treasury proposals. Highlights Include:Yada Yada Yada";
+
+	document.getElementById("eduBlurbTwo").classList.add('nothidden');
+	document.getElementById("eduBlurbTwo").classList.remove('hidden');
+
+
+	document.getElementById("eduIntroBlurb").classList.add('hidden');
+	document.getElementById("eduIntroBlurb").classList.remove('nothidden');
+	document.getElementById("eduBlurbOne").classList.add('hidden');
+	document.getElementById("eduBlurbOne").classList.remove('nothidden');
+	document.getElementById("eduBlurbThree").classList.add('hidden');
+	document.getElementById("eduBlurbThree").classList.remove('nothidden');
+
+}
+
+
+
+
+function eduClickThree() {
+	//document.getElementById("aboutBox").innerHTML = "Wouldn't You Like to Know";
+
+	document.getElementById("eduBlurbThree").classList.add('nothidden');
+	document.getElementById("eduBlurbThree").classList.remove('hidden');
+
+
+	document.getElementById("eduIntroBlurb").classList.add('hidden');
+	document.getElementById("eduIntroBlurb").classList.remove('nothidden');
+	document.getElementById("eduBlurbOne").classList.add('hidden');
+	document.getElementById("eduBlurbOne").classList.remove('nothidden');
+	document.getElementById("eduBlurbTwo").classList.add('hidden');
+	document.getElementById("eduBlurbTwo").classList.remove('nothidden');
+
+
+}
+
+////////////
+/ Skills  /
+///////////
+
+
+function skillClickOne() {
+	//document.getElementById("aboutBox").innerHTML = "While working as a Client Service Advisor, I directly aided a corporate client in recovering over $100,000 in stolen funds after the CEO's email was compromised. That incident drew my interest towards Information Security. That interest became a hobby and then a passion. In 2020 I decided to realign my career towards tech while leveraging self-learning, traditional education, and 5+ years of corporate finance experience.";
+	//document.getElementById("aboutIntroBlurb").element.className = "hidden";
+	document.getElementById("skillBlurbOne").classList.add('nothidden');
+	document.getElementById("skillBlurbOne").classList.remove('hidden');
+
+
+	document.getElementById("skillIntroBlurb").classList.add('hidden');
+	document.getElementById("skillIntroBlurb").classList.remove('nothidden');
+	document.getElementById("skillBlurbTwo").classList.add('hidden');
+	document.getElementById("skilBlurbTwo").classList.remove('nothidden');
+	document.getElementById("skillBlurbThree").classList.add('hidden');
+	document.getElementById("skillBlurbThree").classList.remove('nothidden');
+
+	
+}
+
+function skillClickTwo() {
+	//document.getElementById("aboutBox").innerHTML = "Before learning to pursue a tech-focused career, I was a finance professional. I started as a part-time teller. Within five years, I was traveling across the states to meet with high-profile commercial clients and present treasury proposals. Highlights Include:Yada Yada Yada";
+
+	document.getElementById("skillBlurbTwo").classList.add('nothidden');
+	document.getElementById("skillBlurbTwo").classList.remove('hidden');
+
+
+	document.getElementById("skillIntroBlurb").classList.add('hidden');
+	document.getElementById("skillIntroBlurb").classList.remove('nothidden');
+	document.getElementById("skillBlurbOne").classList.add('hidden');
+	document.getElementById("skillBlurbOne").classList.remove('nothidden');
+	document.getElementById("skillBlurbThree").classList.add('hidden');
+	document.getElementById("skillBlurbThree").classList.remove('nothidden');
+
+}
+
+function skillClickThree() {
+	//document.getElementById("aboutBox").innerHTML = "Wouldn't You Like to Know";
+
+	document.getElementById("skillBlurbThree").classList.add('nothidden');
+	document.getElementById("skillBlurbThree").classList.remove('hidden');
+
+
+	document.getElementById("skillIntroBlurb").classList.add('hidden');
+	document.getElementById("skillIntroBlurb").classList.remove('nothidden');
+	document.getElementById("skillBlurbOne").classList.add('hidden');
+	document.getElementById("skillBlurbOne").classList.remove('nothidden');
+	document.getElementById("skillBlurbTwo").classList.add('hidden');
+	document.getElementById("skillBlurbTwo").classList.remove('nothidden');
+
+
+}
